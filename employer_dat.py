95ad3a87effed62615a8a19aa2e198d453aec25a
Christopher Vredenburgh <cvredenburgh@Christophers-MacBook-Air.local>
2021-11-24 09:55:00 -0500

    short changes

--- employer_dat.py
+++ employer_dat.py
@@ -2,6 +2,20 @@
 #
 # This script serves to process and clean the WCC participant data
 #
-
+# Load relevant packages
 import pandas as pd
 import numpy as np
+print(pd._version)
+
+# Import Baseline Participant Information
+file_folder = '/Users/cvredenburgh/Downloads/';
+res = 'residents_baseline_211013.csv';
+emp = 'employers_baseline_211013.csv';
+res = pd.read_csv(file_folder + res);
+emp = pd.read_csv(file_folder + emp);
+
+# Update column titles and data types to support analysis and cleaning
+for i in []:
+
+# Assess missing customer data to target
+print(res.dtypes)
